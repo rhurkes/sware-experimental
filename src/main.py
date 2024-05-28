1339ed189ef6efc530f9fd8a68faa94024b912d2
Robert Hurkes <1540439+rhurkes@users.noreply.github.com>
2024-05-28 18:36:08 -0500

    wip

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
-from outlook_client import get_tornado_risks
+# from outlook_client import get_tornado_risks
 
 # Day 1 outlooks are available at 0600z, 1300z, 1630z, 2000z
-hour = '1300'
-date = '2022-03-05'
-risks = get_tornado_risks(date, hour)
-print(risks)
+# hour = '1300'
+# date = '2022-03-05'
+# risks = get_tornado_risks(date, hour)
+# print(risks)
+
+from bottle import route, run, template
+import asyncio
+
+@route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+run(host='localhost', port=8080
+
